8be5458cac4cf8143686bd801799f73899b11dc8
zingeer <55106456+zingeer@users.noreply.github.com>
2021-05-04 16:28:42 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,39 +2,20 @@ plugins {
     kotlin("jvm") version "1.4.32"
     maven
 }
-allprojects {
-    apply(plugin = "kotlin")
 
-    repositories {
-        jcenter()
-        maven ("https://jitpack.io/")
-        maven("https://papermc.io/repo/repository/maven-public/")
-        maven("https://gitlab.com/XjCyan1de/maven-repo/-/raw/master/")
+repositories {
+    jcenter()
+    maven("https://gitlab.com/XjCyan1de/maven-repo/-/raw/master/")
+    maven ("https://papermc.io/repo/repository/maven-public/")
+}
 
-    }
+dependencies {
+    implementation(kotlin("stdlib"))
+    compileOnly("com.destroystokyo.paper", "paper-api", "1.16.5-R0.1-SNAPSHOT")
+    compileOnly("org.yatopiamc:yatopia:1.16.4-R0.1-SNAPSHOT")
+}
 
-    dependencies {
-        compileOnly("com.destroystokyo.paper", "paper-api", "1.16.5-R0.1-SNAPSHOT")
-        compileOnly("org.yatopiamc:yatopia:1.16.4-R0.1-SNAPSHOT")
-    }
-
-    tasks {
-        val kotlinOptions: org.jetbrains.kotlin.gradle.dsl.KotlinJvmOptions.() -> Unit = {
-            jvmTarget = "15"
-            freeCompilerArgs = listOf(
-                "-Xjvm-default=all",
-                "-Xopt-in=kotlin.RequiresOptIn"
-            )
-        }
-        java {
-            sourceCompatibility = JavaVersion.VERSION_15
-            targetCompatibility = JavaVersion.VERSION_15
-        }
-        compileKotlin {
-            kotlinOptions(kotlinOptions)
-        }
-        compileTestKotlin {
-            kotlinOptions(kotlinOptions)
-        }
-    }
+tasks {
+    compileKotlin { kotlinOptions.jvmTarget = "1.8" }
+    compileJava { options.encoding = "UTF-8" }
 }
